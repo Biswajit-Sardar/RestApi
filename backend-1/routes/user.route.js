8fa354896d4a5f31e1da9f186d8c2616e4d73088
Biswajit Sardar <biswajitsardar04082005@gmail.com>
2026-09-24 17:03:24 +0530

    Implement user controller methods and update routes for CRUD operations

--- backend-1/routes/user.route.js
+++ backend-1/routes/user.route.js
@@ -1,9 +1,10 @@
 const router = require ('express').Router();
+const userController = require('../controllers/User.controller');
 
-router.get('/api/users',);
-router.post('/api/users',);
-router.get('/api/users/:id',);
-router.put('/api/users/:id',);
-router.delete('/api/users/:id',);
+router.get('/api/users',userController.getAllUsers);
+router.post('/api/users',userController.createUser);
+router.get('/api/users/:id',userController.getUserById);
+router.put('/api/users/:id',userController.updateUserById);
+router.delete('/api/users/:id',userController.deleteUserById);
 
 module.exports = router;
