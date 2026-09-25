70a3b9c6d95726b5866d28612f19e6dcfabbf5a8
Biswajit Sardar <biswajitsardar04082005@gmail.com>
2026-09-25 15:55:07 +0530

    Update server port configuration to 5000 and add User model

---
+++ backend-2/models/User.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {name:{type:String, required:true, trim:true},
+    email:{type:String, required:true, unique:true,lowercase: true, trim:true},
+    role:{type:String, enum:['admin','user'], default:'user'},
+});
+userSchema.pre('save', function (next) {
+    this.updatedAt = Date.now();
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- backend-2/server.js
+++ backend-2/server.js
@@ -3,7 +3,7 @@ const {connectDB}= require('./config/db.mongo');
 
 const app = express();
 
- const PORT = process.env.PORT || 8000;
+ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
