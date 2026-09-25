d3584a6399e031c4dcab9322ffc1f0a5d807c1fe
Biswajit Sardar <biswajitsardar04082005@gmail.com>
2026-09-25 17:38:54 +0530

    Implement user and product routes with CRUD operations and health check endpoints

---
+++ backend-2/routes/healthRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+const healthController = require('../controllers/healthController');
+
+router.get('/health', healthController.getHealth);
+router.get('/version', healthController.getAppVersion);
+
+module.exports = router;
+
--- backend-2/server.js
+++ backend-2/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const {connectDB}= require('./config/db.mongo');
 
+const healthRoutes = require('./routes/healthRoutes');
+const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/productRoutes');
+
 const app = express();
 
- const PORT = process.env.PORT || 5000;
+app.use('/api', healthRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
