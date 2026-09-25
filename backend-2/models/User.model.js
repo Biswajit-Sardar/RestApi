0334f94411eab3d7b19c31ef87b4e3809548a2d1
Biswajit Sardar <biswajitsardar04082005@gmail.com>
2026-09-25 15:59:39 +0530

    Add user schema fields: isActive, createdAt, and updatedAt

--- backend-2/models/User.model.js
+++ backend-2/models/User.model.js
@@ -4,6 +4,9 @@ const userSchema = new mongoose.Schema(
     {name:{type:String, required:true, trim:true},
     email:{type:String, required:true, unique:true,lowercase: true, trim:true},
     role:{type:String, enum:['admin','user'], default:'user'},
+    isActive:{type:Boolean, default:true},
+    createdAt:{type:Date, default:Date.now},
+    updatedAt:{type:Date, default:Date.now}
 });
 userSchema.pre('save', function (next) {
     this.updatedAt = Date.now();
