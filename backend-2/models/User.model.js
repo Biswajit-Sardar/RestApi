360c3f0f7c4bd39be70e3c4101858aff4369d8e3
Biswajit Sardar <biswajitsardar04082005@gmail.com>
2026-09-25 16:34:58 +0530

    Add Product model and app version utility

--- backend-2/models/User.model.js
+++ backend-2/models/User.model.js
@@ -13,6 +13,7 @@ userSchema.pre('save', function (next) {
     next();
 });
 
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
---
+++ backend-2/utils/appVersion.js
@@ -0,0 +1,8 @@
+
+const APP_VERSION = "1.0.0";
+const DEPLOYD_AT = new Date().toISOString();
+
+module.exports = {
+    APP_VERSION,
+    DEPLOYD_AT
+};
