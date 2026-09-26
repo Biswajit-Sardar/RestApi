132a719297dd887ea165d460a8fe320908cf9112
Biswajit Sardar <biswajitsardar04082005@gmail.com>
2026-09-26 15:00:21 +0530

    Refactor product model and update user routes for consistency

--- backend-2/routes/healthRoutes.js
+++ backend-2/routes/healthRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const healthController = require('../controllers/healthController');
 
 router.get('/health', healthController.getHealth);
-router.get('/version', healthController.getAppVersion);
+router.get('/version', healthController.getVersion);
 
 module.exports = router;
 
--- backend-2/routes/userRoutes.js
+++ backend-2/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express= require('express');
 const router= express.Router();
-const userController= require('../controllers/User.controller');
+const userController= require('../controllers/userController');
 
 
 router.get('/users', userController.getAllUsers);
